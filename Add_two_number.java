a90ea38511033c8e0000626eaabfebbd7f2594e3
sachin shukla <sachinshukla232003@gmail.com>
2025-01-07 23:38:25 +0530

    add Add two numbers code

---
+++ Add_two_number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Add_two_number{
+		public static void main(String [] args){
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter your first number");
+			int num1=sc.nextInt();
+			System.out.println("Enter your second number");
+			int num2=sc.nextInt();
+			System.out.println("sum = "+ (num1+num2));
+	}
+}
