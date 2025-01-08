50ade72d301e7e3a16a23b66f0dabac086a66a42
sachin shukla <sachinshukla232003@gmail.com>
2025-01-08 10:35:48 +0530

    add Perimeter Of a Rectangle

---
+++ PerimeterOfaRectangle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class PerimeterOfaRectangle{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the length: ");
+		double length = sc.nextDouble();
+		System.out.print("Enter the width: ");
+		double width= sc.nextDouble();
+		double premiter = 2*(length +  width);
+		System.out.println("Perimeter Of a Rectangle: " + premiter);
+	}
+}
